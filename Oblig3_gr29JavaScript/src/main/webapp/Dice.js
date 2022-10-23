00c91e85e2052e11deea1d93e84ca66ede2182f2
591358 <jan-petter96@hotmail.com>
2022-10-23 14:21:51 +0200

    Added comments to JS code

--- Oblig3_gr29JavaScript/src/main/webapp/Dice.js
+++ Oblig3_gr29JavaScript/src/main/webapp/Dice.js
@@ -3,12 +3,19 @@
  */
  
 export class Dice{
-	constructor(value){
-		this.value=value
-	}
+	//By not including a constructor, JS will provide us with a standard constructor
+	//Constructor (){}
+	
+	/**
+	 * Will return the value of the dice after rolling it
+	 */
 	getValue(){
 		return this.roll();
 	}
+	
+	/**
+	 * Will simulate a roll of a dice by using the Math.random() function
+	 */
 	roll(){
 		return Math.floor(Math.random() *6)+1;;
 	}
--- Oblig3_gr29JavaScript/src/main/webapp/DiceController.js
+++ Oblig3_gr29JavaScript/src/main/webapp/DiceController.js
@@ -5,22 +5,45 @@
 
 import { Dice } from './Dice.js';
 class DiceController {
+	//# -> the method or variable is private	
 	#rootElement
+	
+	/**
+	 * Constructor for creating a DiceController object. The constructor takes a "root" element, i.e. an accessible element
+	 * from the html. When the object is created we also add an eventlistener to a button with the value [data-dicebutton].
+	 */
 	constructor(rootElement) {
 		this.#rootElement = rootElement;
+		
+		
+		//Adding event listener to the "[data-dicebutton]" button. Whenever the button is clicked the #rollDice method will be called
 		const dicebutton = rootElement.querySelector("*[data-dicebutton]");
 		dicebutton.addEventListener("click", this.#rollDice.bind(this));
 	}
-
+	
+	/**
+	 * This method selects the span with value of [data-diceoutput], and adds the result of rolling a dice to the inner html
+	 */
+	
 	#rollDice() {
-		const diceoutputElement = this.#rootElement.querySelector("*[data-diceoutput]");
+		const diceoutputElement = this.#rootElement.querySelector("*[data-diceoutput]");		
+		//Probably bad practice to use new Dice.getValue() here
+		//const dice = new Dice();
+		//diceoutPutElement.innerText = dice.getValue(); is an alternative.
 		diceoutputElement.innerText = new Dice().getValue();
 	};
 }
-
+/**
+ * Function to initialize the Dice Controller through a root element.
+ * The method accesses the HTML element with id "root", and creates a new intstance of DiceCrontroller using said element.
+ */
 function init() {
 	const rootElement = document.getElementById('root');
 	new DiceController(rootElement);
 }
+
+/**
+ * Runs the script -> same as addEventListener('DOMContentLoaded', init());
+ */
 
 init();
